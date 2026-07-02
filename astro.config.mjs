947690f6e687eaa89ed7a56d3fd1d1bd00da099f
Wang Jiangyu <wangjiangyu@Wangs-MacBook-Air-4.local>
2026-07-03 01:07:28 +0800

    Version built asset filenames

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,13 @@ import { defineConfig } from 'astro/config';
 
 export default defineConfig({
   site: 'https://hkcompanylaw.eastlaw.wang/',
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          assetFileNames: '_astro/[name]-v20260703.[hash][extname]',
+        },
+      },
+    },
+  },
 });
